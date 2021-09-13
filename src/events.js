ee3cfa62da87c6dbb1476a40f849778e109a5938
Franklin Perseu de Lima e Lima <69854802+franklinperseuDS@users.noreply.github.com>
2021-09-13 16:10:35 -0400

    first Commit

---
+++ src/events.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+function createRouter(db) {
+    const router = express.Router();
+    const owner = "";
+
+
+    //the router are defined here
+
+    return router;
+}
+
+module.exports = createRouter;
+
+
+
+router.post('/event', (req, res, next) => {
